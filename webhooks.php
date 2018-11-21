2c918a25e052d19786234d01248634e0bdba6854
kaweew <kawee.tee@gmail.com>
2018-11-21 16:38:52 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			if (0 == strcasecmp($event['message']['text'], 'id')) {
-				$text = 'Your ID is' . $event['source']['userId'];
+				$text = 'Your ID is ' . $event['source']['userId'];
 			}
 			else {
 				$text = $event['message']['text'] . " จ๊ะ";
