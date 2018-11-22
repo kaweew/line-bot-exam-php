f2fbcd863d98ef26e424abb6ebc4bef2cc52a3b8
Kawee Wattanaviroon <kawee.tee@gmail.com>
2018-11-22 15:59:46 +0700

    Update linebotpush.php

--- linebotpush.php
+++ linebotpush.php
@@ -14,6 +14,7 @@ if (!is_null($events['events'])) {
     // Loop through each event
     foreach ($events['events'] as $event) {
         // Reply only when message sent is in 'text' format
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
             // Get Source Device
             $pushID = $event['pushID'];
             // Get text sent
