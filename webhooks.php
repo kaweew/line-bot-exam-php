6dcd74ec4fdc5100fd3736d2ff92fb49f3ceeddf
kaweew <kawee.tee@gmail.com>
2018-11-22 14:02:42 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -11,46 +11,66 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			if (0 == strcasecmp($event['message']['text'], 'id')) {
-				$text = 'Your ID is ' . $event['source']['userId'];
-			}
-			else {
-				$text = $event['message']['text'] . " จ๊ะ";
-			}
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+    // Loop through each event
+    foreach ($events['events'] as $event) {
+        // Reply only when message sent is in 'text' format
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+            // Get text sent
+            if (0 == strcasecmp($event['message']['text'], 'id')) {
+                if(isset($event['source']['room']){
+                    $text = 'Your ID is ' . $event['source']['userId'];
+                }
+                else {
+                    $text = "No User ID information.";
+                }
+            }
+            else if (0 == strcasecmp($event['message']['text'], 'group')) {
+                if(isset($event['source']['room']){
+                    $text = 'Group ID is ' . $event['source']['groupId'];
+                }
+                else {
+                    $text = "No Group ID information.";
+                }
+            }
+            else if (0 == strcasecmp($event['message']['text'], 'room')) {
+                if(isset($event['source']['groupId']){
+                    $text = 'Room ID is ' . $event['source']['room'];
+                }
+                else {
+                    $text = "No Room ID information.";
+                }
+            }
+        }
+        else {
+            $text = $event['message']['text'] . " จ๊ะ";
+        }
+        // Get replyToken
+        $replyToken = $event['replyToken'];
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
+        // Build message to reply back
+        $messages = [
+            'type' => 'text',
+            'text' => $text
+        ];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+        // Make a POST Request to Messaging API to reply to sender
+        $url = 'https://api.line.me/v2/bot/message/reply';
+        $data = [
+            'replyToken' => $replyToken,
+            'messages' => [$messages],
+        ];
+        $post = json_encode($data);
+        $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
-	}
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        echo $result . "\r\n";
+    }
 }
 echo "OK";
