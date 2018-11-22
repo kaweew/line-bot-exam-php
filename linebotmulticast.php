fa5b388b20d14be2ca2a76ff46009bc4a3c3f5b6
kaweew <kawee.tee@gmail.com>
2018-11-22 10:40:20 +0700

    Update linebotmulticast.php

--- linebotmulticast.php
+++ linebotmulticast.php
@@ -1,17 +1,18 @@
 <?php
-
-
-
 require "vendor/autoload.php";
 
 $access_token = 'ZRy9NdUUIyNUC5V/Ka8tbgxZWTOejy+8EFsdUAfvdsC8Y+xvO8wC7gmSkAgIFFc6Gi5W3MprNVi8EUv52qSgWwm2xwwlLLG+ym0EN2DzDq0RqV1IkcYPM32JD4KVeaQTQWPyGJIY4Sk4x4X4khzrHgdB04t89/1O/w1cDnyilFU=';
-
 $channelSecret = '70caa8313776365f76a9521a9abfc3f8';
-
 $multicastPushIDs = array(
     'U842c2884dad3c4fda6e844e35b33dc29'    // Kawee
 );
 
+// Get POST body content
+$content = file_get_contents('php://input');
+echo $condtent
+// Parse JSON
+//$events = json_decode($content, true);
+/*
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
@@ -19,3 +20,4 @@ $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello
 $response = $bot->multicast($multicastPushIDs, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+*/
