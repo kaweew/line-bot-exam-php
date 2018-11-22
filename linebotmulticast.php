bf1bfe7dffd8add96abf900536282c101544255d
kaweew <kawee.tee@gmail.com>
2018-11-22 14:27:59 +0700

    Update linebotmulticast.php

--- linebotmulticast.php
+++ linebotmulticast.php
@@ -4,8 +4,8 @@ require "vendor/autoload.php";
 $access_token = 'ZRy9NdUUIyNUC5V/Ka8tbgxZWTOejy+8EFsdUAfvdsC8Y+xvO8wC7gmSkAgIFFc6Gi5W3MprNVi8EUv52qSgWwm2xwwlLLG+ym0EN2DzDq0RqV1IkcYPM32JD4KVeaQTQWPyGJIY4Sk4x4X4khzrHgdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '70caa8313776365f76a9521a9abfc3f8';
 $multicastPushIDs = array(
-    'U842c2884dad3c4fda6e844e35b33dc29',    // Kawee
-    'Cd0d18c6d9d8c351987a81cf9c61f067c'	    // Group System Monitor
+    'U842c2884dad3c4fda6e844e35b33dc29'    // Kawee
+    //'Cd0d18c6d9d8c351987a81cf9c61f067c'	    // Group System Monitor
 );
 
 // Get POST body content
